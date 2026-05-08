18cda10517a316fd58ab61b2bb446a578053de30
Ramatovo <49375369@est.ort.edu.ar>
2026-05-08 14:24:28 -0300

    termine

--- dap-login/lib/go_router.dart
+++ dap-login/lib/go_router.dart
@@ -1,17 +1,17 @@
-import 'package:dap_login/home.dart';
-import 'package:dap_login/login.dart';
+import 'package:flutter/material.dart';
+import 'home.dart';
+import 'login.dart';
 import 'package:go_router/go_router.dart';
-
 final appRouter = GoRouter(
   initialLocation: '/login',
   routes: [
     GoRoute(
       path: '/login',
-      builder: (context, state) => const Login(),
+      builder: (context, state) => const login(),
     ),
     GoRoute(
       path: '/home',
-      builder: (context, state) => const Home(),
+      builder: (context, state) => const home(),
     ),
   ]
 );
