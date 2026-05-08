e49845b6c1408130f7e6498cfa97fc31e45f471b
Ramatovo <49375369@est.ort.edu.ar>
2026-05-08 14:24:51 -0300

    termine

--- dap-login/lib/home.dart
+++ dap-login/lib/home.dart
@@ -1,13 +1,14 @@
+
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class home extends StatelessWidget {
+  const home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("bienvenido"),
+        title:  Text("bienvenido"),
       ),
     );
   }
